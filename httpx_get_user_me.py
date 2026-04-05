99e0434652dd81f39a7af4b8b0b249c27936e7b8
Minnebaev-il <iw@qaen.ru>
2026-04-06 01:18:31 +0300

    Добавил вывод статус кода

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -9,8 +9,10 @@ with httpx.Client() as client:
     login = client.post("http://localhost:8000/api/v1/authentication/login", json=payload)
     login.raise_for_status()
     print(login.json())
+    print(login.status_code)
     token = login.json().get("token", {}).get("accessToken")
     headers = {"Authorization": f"Bearer {token}"}
     me = client.get("http://localhost:8000/api/v1/users/me", headers=headers)
     me.raise_for_status()
     print(me.json())
+    print(me.status_code)
