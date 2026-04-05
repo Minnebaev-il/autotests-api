b790d52fd6f58055bf1c507e12de2bb2e34ab3c1
Minnebaev-il <iw@qaen.ru>
2026-04-06 01:12:51 +0300

    Написал скрипт на авторизацию и последующего получения информации о пользователе

---
+++ httpx_get_user_me.py
@@ -0,0 +1,16 @@
+import httpx
+
+
+payload = {
+        "email": "iw@qaen.ru",
+        "password": "Dream=007!"
+}
+with httpx.Client() as client:
+    login = client.post("http://localhost:8000/api/v1/authentication/login", json=payload)
+    login.raise_for_status()
+    print(login.json())
+    token = login.json().get("token", {}).get("accessToken")
+    headers = {"Authorization": f"Bearer {token}"}
+    me = client.get("http://localhost:8000/api/v1/users/me", headers=headers)
+    me.raise_for_status()
+    print(me.json())
