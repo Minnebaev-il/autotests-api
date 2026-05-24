ee49c9a79e7f42c4c687986ba8eddfa56415933e
Minnebaev-il <iw@qaen.ru>
2026-05-24 13:48:43 +0300

    8.5 Фикстуры Pytest

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,6 +1,12 @@
 from typing import Any
 
 def assert_status_code(actual: int, expected: int):
+    """
+    Проверяет, что фактический статус-код ответа соответствует ожидаемому.
+
+    :param actual: Фактический статус-код ответа.
+    :param expected: Ожидаемый статус-код.
+    """
     assert actual == expected, (
         'Incorrect response status code. '
         f'Expected status code: {expected}. '
@@ -9,6 +15,13 @@ def assert_status_code(actual: int, expected: int):
 
 
 def assert_equal(actual: Any, expected: Any, name: str):
+    """
+    Проверяет, что фактическое значение равно ожидаемому.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :param expected: Ожидаемое значение.
+    """
     assert actual == expected, (
         f'Incorrect value: "{name}". '
         f'Expected value: "{expected}". '
