10c739ac5e0cfcf02dbe09c1cf0c59ec3c091a02
Minnebaev-il <iw@qaen.ru>
2026-06-04 21:19:23 +0300

    10.2 Интеграция Allure в автотесты

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,7 +1,13 @@
 from http import HTTPStatus
 
 import pytest
+import allure
+from allure_commons.types import Severity
 
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, GetCoursesQuerySchema, \
     GetCoursesResponseSchema, CreateCourseRequestSchema, CreateCourseResponseSchema
@@ -16,7 +22,14 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.COURSES, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
 class TestCourses:
+    @allure.tag(AllureTag.GET_ENTITIES)
+    @allure.story(AllureStory.GET_ENTITIES)
+    @allure.title("Get course")
+    @allure.severity(Severity.BLOCKER)
     def test_get_courses(
             self, 
             courses_client: CoursesClient, 
@@ -32,7 +45,10 @@ class TestCourses:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.title("Create course")
+    @allure.severity(Severity.BLOCKER)
     def test_create_course(
             self,
             courses_client: CoursesClient,
@@ -50,7 +66,10 @@ class TestCourses:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-        
+    @allure.tag(AllureTag.UPDATE_ENTITY)
+    @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.title("Update course")
+    @allure.severity(Severity.CRITICAL)
     def test_update_course(self, courses_client: CoursesClient, function_course: CourseFixture):
         request = UpdateCourseRequestSchema()
         response = courses_client.update_course_api(function_course.response.course.id, request)
