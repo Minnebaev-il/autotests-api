5479e001e09777cde75c04acfeac11cb556e112f
Minnebaev-il <Minnebaev-il@users.noreply.github.com>
2026-07-05 21:03:44 +0000

    deploy: 466fd633bb9ec5eff8e6181150a9daebeccec63e

--- fixtures/authentication.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from clients.authentification.authentification_client import AuthenticationClient, get_authentication_client
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
-
